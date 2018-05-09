9682e25f8fe8685a5da70c06aeab750d091e707b
RUPALI <rupali123@gmail.com>
2018-05-09 17:08:35 +0530

    homepage download change

--- src/Home/Event.js
+++ src/Home/Event.js
@@ -115,6 +115,13 @@ export default class Event extends Component{
 
                             />
 
+                            <section className="">
+                                <h1 className="line">Announcement</h1>
+                                <div className="section-content">
+                                              {events}
+                                    {/*<a className="read-more" href="/News">All events<i className="fa fa-chevron-right"></i></a>*/}
+                                </div>
+                            </section>
 
                          {/*   <DayPicker                            
 
@@ -124,8 +131,8 @@ export default class Event extends Component{
                             /> */}
 
         </div>
-        <div className="col-md-5">
-            <blockquote className="custom-quote" style={{"padding":"0 0 0 20px"}}>
+        <div className="col-md-6">
+           <blockquote className="custom-quote" style={{"width":"100%"}}>
                    {/* <img src="https://res.cloudinary.com/dvl9i5pry/image/upload/v1505979469/soundarya-manjappa-chairman_wiehap.jpg" style={{"margin-bottom":"20px",height: "193px",width: "100%"}}/> 
                     
                     <p style={{"text-align":"justify"}}><i className="fa fa-quote-left"></i>&nbsp;<strong>Desire</strong> is the key to motivation; but it is stern determination &amp; commitment to an unrelenting pursuit of your goal – a commitment to excellence  that will enable you to attain the success you seek “ . I am tremendously delighted to witness the colourful strides we could accomplish during the last 23 years of our proud march towards <strong><u>‘Destination Excellence’</u></strong> I wish that our vision statement involve and evolve becomes true in the days to come. </p>
@@ -135,29 +142,110 @@ export default class Event extends Component{
                         <span className="title">Chairman</span>
                         </strong>
                     </p> */}
-                    <section className="">
-                        <h1 className="line">Announcement</h1>
-                        <div className="section-content">
-                                      {events}
-                            {/*<a className="read-more" href="/News">All events<i className="fa fa-chevron-right"></i></a>*/}
+
+                   
+                     <h1 style={{"text-align":"center","color":"#167ac6","fontsize":"24"}}>Soundarya School</h1><br/>
+                                <p style={{"text-align":"justify","color":"#167ac6"}}><i className="fa fa-quote-left"></i>
+                                 Born in the year 1993, Rocking on the cradle of progress incredible to the core…….
+                                 Spinning a shinning tomorrow on the wings of faith, Triumphant in the grace of Sacred Soundarya
+                                 Lahari.<br/>
+                                 We lead and others follow Where wisdom flows in abundance We give our solemn pledge. 
+                                 To uphold the banner of your aspiration The flag of hope and success conscious,Vigilant and progressive.<br/>
+                                 A citadel of academic excellence in building the future of our beloved nation.
+                                 We are out for the best for the sky in our limit.
+                                 We are, we ought,
+                                 We will be proud Soundaryans always.<i className="fa fa-quote-right"></i></p>          
+
+                    
+                       
+            </blockquote>    
+
+            <h1 className="section-heading text-highlight"><span className="line">Photo Gallery</span></h1>
+          <div className="section-content">
+            <div className="row">
+                            <div className="col-md-4" >
+                                <img className="img-1" src="http://zenopsys.net/soundarya_forms/school/images/sch1.jpg" alt="commerece-college-nagasandra" style={{"width":"180px","height":"135px"}}/>
+                            </div>
+                            <div className="col-md-4">
+                                <img  className="img-1" src="http://zenopsys.net/soundarya_forms/school/images/sch2.jpg" alt="top-puc-college-peenya" style={{"width":"180px","height":"135px"}}/>
+                            </div>
+                            <div className="col-md-4">
+                                <img  className="img-1" src="http://zenopsys.net/soundarya_forms/school/images/sch4.jpg" alt="top-pu-college" style={{"width":"180px","height":"135px"}}/>
+                            </div>
+                            <div className="col-md-4">
+                                <img  className="img-1" src="http://zenopsys.net/soundarya_forms/school/images/sch3.jpg" alt="top-pu-college" style={{"height":"150px","width":"180px","padding-top":"15px"}}/>
+                            </div>
+                            <div className="col-md-4">
+                                <img  className="img-1" src="http://zenopsys.net/soundarya_forms/school/images/sch5.jpg" alt="top-pu-college"  style={{"height":"150px","width":"180px","padding-top":"15px"}}/>
+                            </div>
+                            <div className="col-md-4">
+                                <img  className="img-1" src="http://zenopsys.net/soundarya_forms/school/images/sch6.jpg" alt="top-pu-college"  style={{"height":"150px","width":"180px","padding-top":"15px"}}/>
+                            </div>
+                            <br style={{"clear":"both"}}/>
+                            <br style={{"clear":"both"}}/>
+                            <div className="col-md-6">
+                                <a className="read-more" href="/#/Gallery">View All<i className="fa fa-chevron-right"></i></a>
+                            </div>
                         </div>
-                    </section>
-            </blockquote>               
+          </div>
+
+                    <h1 className="section-heading text-highlight"><span className="line">Videos</span></h1>
+                    <div className="section-content">
+                        <div className="row">
+                      
+                            <div className="col-md-4">
+                                <iframe width="100%" height="150" src="https://www.youtube.com/embed/7vZ5w-FgJtY" frameborder="0" allowfullscreen="" style={{"margin-bottom":"10px"}}></iframe>
+                            </div>
+                            <div className="col-md-4">
+                                <iframe width="100%" height="150" src="https://www.youtube.com/embed/OVkJ9_goSWc" frameborder="0" allowfullscreen="" style={{"margin-bottom":"10px"}}></iframe>
+                            </div>
+                           <div className="col-md-4">
+                                 <iframe width="100%" height="150" src="https://www.youtube.com/embed/YvJ1HqcKu9Y" frameborder="0" allowfullscreen="" style={{"margin-bottom":"10px"}}></iframe>
+                            </div>
+                           {/*  <div className="col-md-4">
+                                <iframe width="100%" height="150" src="https://www.youtube.com/embed/ne0t1wWnl80" frameborder="0" allowfullscreen="" style={{"margin-bottom":"10px"}}></iframe>
+                            </div>
+                            <div className="col-md-4">
+                                <iframe width="100%" height="150" src="https://www.youtube.com/embed/ne0t1wWnl80" frameborder="0" allowfullscreen="" style={{"margin-bottom":"10px"}}></iframe>
+                            </div>
+                            <div className="col-md-4">
+                                <iframe width="100%" height="150" src="https://www.youtube.com/embed/ne0t1wWnl80" frameborder="0" allowfullscreen="" style={{"margin-bottom":"10px"}}></iframe>
+                            </div> */}
+                            <br style={{"clear":"both"}}/>
+                            <br style={{"clear":"both"}}/>
+                          {/*  <div className="col-md-6">
+                                <a className="read-more" href="/#/Videos">View All<i className="fa fa-chevron-right"></i></a>
+                            </div> */}
+                       
+                        </div>
+                    </div>
+ 
+                           
 
         </div>
-        <div className="col-md-4">
+        <div className="col-md-3">
             <section className="links">
                 
                
                 <div className="widget facebooklikebox-2 widget_FacebookLikeBox" style={{"padding-top": "0px"}}>
                 <script type="text/javascript" src="http://connect.facebook.net/en_US/all.js#xfbml=1"></script>
-                <span style={{"vertical-align": "bottom", width: "100%",height: "460px"}}>
-                <iframe style={{"visibility": "visible", width: "100%","height": "460px"}}title="fb:like_box Facebook Social Plugin" name="f32edf47a343e32" src="http://www.facebook.com/plugins/like_box.php?app_id=&amp;channel=http%3A%2F%2Fstatic.ak.facebook.com%2Fconnect%2Fxd_arbiter.php%3Fversion%3D28%23cb%3Df2dcaef3d4633f%26domain%3Dwww.soundaryainstitutions.in%26origin%3Dhttp%253A%252F%252Fwww.soundaryainstitutions.in%252Ff37e2d61b1b2702%26relation%3Dparent.parent&amp;header=true&amp;href=https%3A%2F%2Fwww.facebook.com%2FSoundaryaSchool%2F&amp;locale=en_US&amp;sdk=joey&amp;show_faces=true&amp;stream=true&amp;width=360" height="450" width="100%" frameborder="0" scrolling="no"></iframe></span></div>                        
+                <span style={{"vertical-align": "bottom", width: "100%",height: "560px"}}>
+                <iframe style={{"visibility": "visible", width: "100%","height": "560px"}}title="fb:like_box Facebook Social Plugin" name="f32edf47a343e32" src="http://www.facebook.com/plugins/like_box.php?app_id=&amp;channel=http%3A%2F%2Fstatic.ak.facebook.com%2Fconnect%2Fxd_arbiter.php%3Fversion%3D28%23cb%3Df2dcaef3d4633f%26domain%3Dwww.soundaryainstitutions.in%26origin%3Dhttp%253A%252F%252Fwww.soundaryainstitutions.in%252Ff37e2d61b1b2702%26relation%3Dparent.parent&amp;header=true&amp;href=https%3A%2F%2Fwww.facebook.com%2FSoundaryaSchool%2F&amp;locale=en_US&amp;sdk=joey&amp;show_faces=true&amp;stream=true&amp;width=360" height="450" width="100%" frameborder="0" scrolling="no"></iframe></span></div>                        
                
                 <div className="fb-page" data-href="https://www.facebook.com/Soundarya-Central-School-1441884222795577" data-tabs="timeline" data-small-header="false" data-height="470" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"></div>
                 
 
                </section>
+
+                <section className="home-page-section">
+                        <div className="section-content">
+                          <h3 className="title">Downloads</h3>
+                                <p>For your benefit we have compiled in this section some of the key features you should know about Soundarya School.</p>
+                                <p><a className="btn btn-theme" href="http://zenopsys.net/soundarya_forms/school/VENTURA2017-18.pdf"><i className="fa fa-download"></i>Prospectus</a></p>
+                        </div>
+
+                    
+                </section>
              </div>
             </div>
          </div>
